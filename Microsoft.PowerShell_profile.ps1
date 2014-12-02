262caf4131f43fa441b2c24e08aafe95135e3be2
xpando <xpando@gmail.com>
2014-12-02 12:31:44 -0600

    Add alias for sublime text

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -21,6 +21,8 @@ Add-Path "$psscriptroot\scripts"
 
 # Aliases
 Set-Alias which Get-Command
+if (test-path "C:\Program Files\Sublime Text 2\sublime_text.exe") { Set-Alias subl "C:\Program Files\Sublime Text 2\sublime_text.exe" }
+if (test-path "C:\Program Files\Sublime Text 3\sublime_text.exe") { Set-Alias subl "C:\Program Files\Sublime Text 3\sublime_text.exe" }
 
 # Remove problematic aliases
 rm alias:curl
