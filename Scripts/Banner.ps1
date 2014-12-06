51f865c1e2dc9fcb5eb14753a8f62978477084ae
xpando <xpando@gmail.com>
2014-12-06 16:52:14 -0600

    Added network info to banner

--- Scripts/Banner.ps1
+++ Scripts/Banner.ps1
@@ -1,17 +1,20 @@
-$computer  = gwmi Win32_ComputerSystem
-$os        = gwmi Win32_OperatingSystem
-$processor = gwmi Win32_Processor
-$display   = gwmi Win32_DisplayConfiguration
-$uptime    = $os.ConvertToDateTime($os.LocalDateTime) - $os.ConvertToDateTime($os.LastBootUpTime)
+$computer    = gwmi Win32_ComputerSystem
+$os          = gwmi Win32_OperatingSystem
+$processor   = gwmi Win32_Processor
+$display     = gwmi Win32_DisplayConfiguration
+$network     = gwmi Win32_NetworkAdapterConfiguration
+$uptime      = $os.ConvertToDateTime($os.LocalDateTime) - $os.ConvertToDateTime($os.LastBootUpTime)
+$ipAddresses = ($network | where IPAddress |% { $_.IPAddress[0] }) -join ", "
 
 $info = `
     "Computer: ", "$($computer.Model), $($computer.Manufacturer)",
+    "Uptime:   ", "$($uptime.Days)d $($uptime.Hours)h $($uptime.Minutes)m $($uptime.Seconds)s",
     "OS:       ", "$($os.Caption) $($os.OSArchitecture)",
     "Kernel:   ", "$($os.Version)",
     "CPU:      ", "$($processor.Name)",
     "GPU:      ", "$($display.DeviceName)",
-    "Uptime:   ", "$($uptime.Days)d $($uptime.Hours)h $($uptime.Minutes)m $($uptime.Seconds)s",
     "Memory:   ", "$([math]::Truncate($os.FreePhysicalMemory / 1KB)) MB / $([math]::Truncate($computer.TotalPhysicalMemory / 1MB)) MB",
+    "Network:  ", "$ipAddresses",
     "Shell:    ", "PowerShell v$($Host.Version)"
 
 $logo = `
@@ -23,7 +26,7 @@ $logo = `
 "    .oc::'':cccc", "red", " .ooooooooooo.          ", "green",       "$($info[10])", "gray", "$($info[11])`r`n", "white",
 "     .:ccocc.", "blue", "    oooooooooooc           ", "green",      "$($info[12])", "gray", "$($info[13])`r`n", "white",
 "   :oooooooocoo", "blue", "   .coCoCooc.            ", "green",      "$($info[14])", "gray", "$($info[15])`r`n", "white",
-"   oooooooooco.", "blue", " COo. ", "yellow", "''", "green", " .cCc            `r`n", "yellow",
+"   oooooooooco.", "blue", " COo. ", "yellow", "''", "green", " .cCc            ", "yellow", "$($info[16])", "gray", "$($info[17])`r`n", "white",
 "  :ooooooooooc", "blue", " cOCCCCCCCCCC:            `r`n", "yellow",
 "  ooooooooooo.", "blue", " CCCCCCCCCCO:             `r`n", "yellow",
 " cc:.   .:co:", "blue", " oOCCCCCCCCOo              `r`n", "yellow",
