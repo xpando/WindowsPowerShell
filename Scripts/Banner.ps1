30dd122c6576129ad2fbba80f3fe20460235b77b
xpando <xpando@gmail.com>
2014-12-06 17:54:11 -0600

    tweak banner

--- Scripts/Banner.ps1
+++ Scripts/Banner.ps1
@@ -7,7 +7,7 @@ $uptime      = $os.ConvertToDateTime($os.LocalDateTime) - $os.ConvertToDateTime(
 $ipAddresses = ($network | where IPAddress |% { $_.IPAddress[0] }) -join ", "
 
 $info = `
-    "Computer: ", "$($computer.Model), $($computer.Manufacturer)",
+    "Computer: ", "$($env:computername) - $($computer.Model), $($computer.Manufacturer)",
     "Uptime:   ", "$($uptime.Days)d $($uptime.Hours)h $($uptime.Minutes)m $($uptime.Seconds)s",
     "OS:       ", "$($os.Caption) $($os.OSArchitecture)",
     "Kernel:   ", "$($os.Version)",
@@ -18,20 +18,20 @@ $info = `
     "Shell:    ", "PowerShell v$($Host.Version)"
 
 $logo = `
-"          .:::..                        ", "red",                    "$($info[0])",  "gray", "$($info[1])`r`n",  "white",
-"       ccccccccccc.                     ", "red",                    "$($info[2])",  "gray", "$($info[3])`r`n",  "white",
-"      :ccccccccccc", "red", " :Cc.      :c         ", "green",       "$($info[4])",  "gray", "$($info[5])`r`n",  "white",
-"      ccccccccccc.", "red", " ooooooooooo.         ", "green",       "$($info[6])",  "gray", "$($info[7])`r`n",  "white",
-"     :cccccccccc:", "red", " cooooooooooc          ", "green",       "$($info[8])",  "gray", "$($info[9])`r`n",  "white",
-"    .oc::'':cccc", "red", " .ooooooooooo.          ", "green",       "$($info[10])", "gray", "$($info[11])`r`n", "white",
-"     .:ccocc.", "blue", "    oooooooooooc           ", "green",      "$($info[12])", "gray", "$($info[13])`r`n", "white",
-"   :oooooooocoo", "blue", "   .coCoCooc.            ", "green",      "$($info[14])", "gray", "$($info[15])`r`n", "white",
-"   oooooooooco.", "blue", " COo. ", "yellow", "''", "green", " .cCc            ", "yellow", "$($info[16])", "gray", "$($info[17])`r`n", "white",
-"  :ooooooooooc", "blue", " cOCCCCCCCCCC:            `r`n", "yellow",
-"  ooooooooooo.", "blue", " CCCCCCCCCCO:             `r`n", "yellow",
-" cc:.   .:co:", "blue", " oOCCCCCCCCOo              `r`n", "yellow",
-"             .OCCCCCCCCCO.              `r`n", "yellow",
-"               .coCCCoc.                `r`n", "yellow"
+"          .:::..                    `r`n", "red",
+"       ccccccccccc.                 `r`n", "red",
+"      :ccccccccccc", "red", " :Cc.      :c     `r`n", "green",
+"      ccccccccccc.", "red", " ooooooooooo.     ", "green",  "$($info[0])",  "gray", "$($info[1])`r`n",  "white",
+"     :cccccccccc:", "red", " cooooooooooc      ", "green",  "$($info[2])",  "gray", "$($info[3])`r`n",  "white",
+"    .oc::'':cccc", "red", " .ooooooooooo.      ", "green",  "$($info[4])",  "gray", "$($info[5])`r`n",  "white",
+"     .:ccocc.", "blue", "    oooooooooooc       ", "green", "$($info[6])",  "gray", "$($info[7])`r`n",  "white",
+"   :oooooooocoo", "blue", "   .coCoCooc.        ", "green", "$($info[8])",  "gray", "$($info[9])`r`n",  "white",
+"   oooooooooco.", "blue", " COo. ", "yellow", "''", "green", " .cCc        ", "yellow", "$($info[10])", "gray", "$($info[11])`r`n", "white",
+"  :ooooooooooc", "blue", " cOCCCCCCCCCC:        ", "yellow", "$($info[12])", "gray", "$($info[13])`r`n", "white",
+"  ooooooooooo.", "blue", " CCCCCCCCCCO:         ", "yellow", "$($info[14])", "gray", "$($info[15])`r`n", "white",
+" cc:.   .:co:", "blue", " oOCCCCCCCCOo          ", "yellow", "$($info[16])", "gray", "$($info[17])`r`n", "white",
+"             .OCCCCCCCCCO.          `r`n", "yellow",
+"               .coCCCoc.            `r`n", "yellow"
 
 clear
 for ($i = 0; $i -lt $logo.Length; $i+=2) {
