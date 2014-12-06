fab89143ae01a73a525ae51735316396f4252ccd
xpando <xpando@gmail.com>
2014-12-06 16:33:16 -0600

    Add fancy banner to startup

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -29,9 +29,10 @@ rm alias:curl
 rm alias:wget
 
 # Print out a fancy PowerShell Banner :)
-if (which figlet -ErrorAction Ignore) {
-    Write-Host (figlet -f slant "PowerShell $($Host.Version.ToString(2))" | Out-String) -ForegroundColor Cyan
-}
+Banner.ps1
+#if (which figlet -ErrorAction Ignore) {
+#    Write-Host (figlet -f slant "PowerShell $($Host.Version.ToString(2))" | Out-String) -ForegroundColor Cyan
+#}
 
 if (gcm pshazz -ea SilentlyContinue) {
     pshazz init 'xpando' 
---
+++ Scripts/Banner.ps1
@@ -0,0 +1,40 @@
+$computer  = gwmi Win32_ComputerSystem
+$os        = gwmi Win32_OperatingSystem
+$processor = gwmi Win32_Processor
+$display   = gwmi Win32_DisplayConfiguration
+$uptime    = $os.ConvertToDateTime($os.LocalDateTime) - $os.ConvertToDateTime($os.LastBootUpTime)
+
+$info = `
+    "Computer: ", "$($computer.Model), $($computer.Manufacturer)",
+    "OS:       ", "$($os.Caption) $($os.OSArchitecture)",
+    "Kernel:   ", "$($os.Version)",
+    "CPU:      ", "$($processor.Name)",
+    "GPU:      ", "$($display.DeviceName)",
+    "Uptime:   ", "$($uptime.Days)d $($uptime.Hours)h $($uptime.Minutes)m $($uptime.Seconds)s",
+    "Memory:   ", "$([math]::Truncate($os.FreePhysicalMemory / 1KB)) MB / $([math]::Truncate($computer.TotalPhysicalMemory / 1MB)) MB",
+    "Shell:    ", "PowerShell v$($Host.Version)"
+
+$logo = `
+"          .:::..                        ", "red",                    "$($info[0])",  "gray", "$($info[1])`r`n",  "white",
+"       ccccccccccc.                     ", "red",                    "$($info[2])",  "gray", "$($info[3])`r`n",  "white",
+"      :ccccccccccc", "red", " :Cc.      :c         ", "green",       "$($info[4])",  "gray", "$($info[5])`r`n",  "white",
+"      ccccccccccc.", "red", " ooooooooooo.         ", "green",       "$($info[6])",  "gray", "$($info[7])`r`n",  "white",
+"     :cccccccccc:", "red", " cooooooooooc          ", "green",       "$($info[8])",  "gray", "$($info[9])`r`n",  "white",
+"    .oc::'':cccc", "red", " .ooooooooooo.          ", "green",       "$($info[10])", "gray", "$($info[11])`r`n", "white",
+"     .:ccocc.", "blue", "    oooooooooooc           ", "green",      "$($info[12])", "gray", "$($info[13])`r`n", "white",
+"   :oooooooocoo", "blue", "   .coCoCooc.            ", "green",      "$($info[14])", "gray", "$($info[15])`r`n", "white",
+"   oooooooooco.", "blue", " COo. ", "yellow", "''", "green", " .cCc            `r`n", "yellow",
+"  :ooooooooooc", "blue", " cOCCCCCCCCCC:            `r`n", "yellow",
+"  ooooooooooo.", "blue", " CCCCCCCCCCO:             `r`n", "yellow",
+" cc:.   .:co:", "blue", " oOCCCCCCCCOo              `r`n", "yellow",
+"             .OCCCCCCCCCO.              `r`n", "yellow",
+"               .coCCCoc.                `r`n", "yellow"
+
+clear
+for ($i = 0; $i -lt $logo.Length; $i+=2) {
+    $str = $logo[$i]
+    $color = $logo[$i+1]
+    Write-Host $str -f $color -nonewline
+}
+
+Write-Host
