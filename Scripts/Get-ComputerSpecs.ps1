cc6af5dc1d3bfc4c5a3934d640480079a216d37c
David Findley <david.findley@mkcorp.com>
2016-01-21 12:26:04 -0600

    Better error handling and update to include disk specs and ip addresses

--- Scripts/Get-ComputerSpecs.ps1
+++ Scripts/Get-ComputerSpecs.ps1
@@ -1,20 +1,47 @@
+[CmdletBinding()]
 param(
-  [string[]] $ComputerName
+  [Parameter(ValueFromPipeline=$True)]
+  [String[]] $ComputerName = "localhost"
 )
 
-$ComputerName |% {
-  @{
-    System = Get-WmiObject Win32_ComputerSystem -ComputerName $_;
-    Processor = Get-WmiObject Win32_Processor -ComputerName $_;
-  } 
-  
-} |% { 
-  @{ 
-    Name = $_.System.Name;
-    Cores = $_.Processor.NumberOfCores;
-    LogicalProcessors = $_.Processor.NumberOfLogicalProcessors; 
-    Memory = "$([math]::Ceiling($_.System.TotalPhysicalMemory / 1Gb)) GB" 
-  } 
-} |% { 
-    New-Object -TypeName PSObject -Property $_ 
+process {
+  foreach ($name in $ComputerName) {
+      $memory = Get-WmiObject Win32_ComputerSystem -ComputerName $name -ea SilentlyContinue
+      if ($memory) {
+        $memory = "$([math]::Ceiling($memory.TotalPhysicalMemory / 1Gb)) GB"
+      } else {
+        $memory = ""
+      }
+    
+    $cpus = Get-WmiObject Win32_Processor -ComputerName $name -ea SilentlyContinue
+    if ($cpus) {
+      $cpus = $cpus |% { "$($_.DeviceID):$($_.NumberOfCores):$($_.NumberOfLogicalProcessors)" }
+      $cpus = $cpus -join "; "
+    } else {
+      $cpus = ""
+    }
+    
+    $disks = Get-WmiObject Win32_logicaldisk -ComputerName $name -ea SilentlyContinue
+    if ($disks) {
+      $disks = $disks |? { $_.DeviceID -in "C:","D:" } | select DeviceID,FreeSpace,Size |% { "$($_.DeviceID) $([math]::Ceiling(($_.Size - $_.FreeSpace) / 1Gb))/$([math]::Ceiling($_.Size / 1Gb)) GB" }
+      $disks = $disks -join "; "
+    } else {
+      $disks = ""
+    }
+    
+    $nics = Get-WmiObject Win32_NetworkAdapterConfiguration -filter "ipenabled = 'true'" -ComputerName $name -ea SilentlyContinue
+    $br = ""
+    $fr = ""
+    if ($nics) {
+      foreach ($nic in $nics) {
+        $adapter = $nic.GetRelated('win32_NetworkAdapter')
+        switch -regex ($adapter.NetConnectionID) {
+          "^Nic\d+-BR-" { $br = $nic.IPAddress }
+          "^Nic\d+-FR-" { $fr = $nic.IPAddress }
+        }
+      }
+    }
+    
+    Write-Output (New-Object -TypeName PSObject -Property @{ Name = $name; Memory = $memory; CPU = $cpus; Disk = $disks; FrontRail = $fr; BackRail = $br })
+  }
 }
