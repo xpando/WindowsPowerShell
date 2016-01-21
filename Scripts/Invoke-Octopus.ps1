c147f54f78ad1da8a72cc42bb0e8aee722b401d8
David Findley <david.findley@mkcorp.com>
2016-01-21 17:43:21 -0600

    Add parameter to specify Octopus host

--- Scripts/Invoke-Octopus.ps1
+++ Scripts/Invoke-Octopus.ps1
@@ -4,11 +4,14 @@ param(
   $ApiKey,
   
   [String]
-  $Path = "/api"
+  $Path = "/api",
+  
+  [String]
+  $OctopusHost = "deploy"
 )
 
 if (-not $Path.StartsWith("/")) { $Path = "/$Path" }
 if (-not $Path.StartsWith("/api")) { $Path = "/api$Path" }
-$url = "http://deploy$Path"
+$url = "http://$OctopusHost$Path"
 $headers = @{ "X-Octopus-ApiKey" = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($apikey)) } 
 Invoke-RestMethod -Method Get -Uri $url -Headers $headers
