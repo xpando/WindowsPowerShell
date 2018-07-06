0394ce68b5f65ace97bd9a19528d1e1b68e9aa08
David Findley <xpando@gmail.com>
2018-07-06 14:33:28 -0700

    Cleanup

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,22 +1,3 @@
-# Hack around corporate IT
-if ($home -eq "M:\") {
-  Set-Variable -Name HOME -Value "$env:systemdrive\users\$env:username" -Force
-  Set-Variable -Name HOMEDRIVE -Value "$env:systemdrive" -Force
-  Set-Variable -Name HOMEPATH -Value "\users\$env:username" -Force
-  (Get-PSProvider filesystem).home = $home
-}
-
-# Modules
-if ($host.Version.Major -lt 5) { Import-Module PsGet }
-Import-Module PSReadLine
-Import-Module PathUtils
-Import-Module VisualStudio
-Import-Module go
-
-if ($host.Name -eq 'ConsoleHost') {
-    Import-Module PSReadline -ErrorAction SilentlyContinue
-}
-
 # Paths
 Add-Path "$psscriptroot\scripts"
 
@@ -29,12 +10,9 @@ if (test-path "C:\Program Files\Sublime Text 3\sublime_text.exe") { Set-Alias su
 rm alias:curl
 rm alias:wget
 
+if (gcm pshazz -ea SilentlyContinue) {
+    pshazz init 'xpando'
+}
+
 # Print out a fancy PowerShell Banner :)
 Banner.ps1
-#if (which figlet -ErrorAction Ignore) {
-#    Write-Host (figlet -f slant "PowerShell $($Host.Version.ToString(2))" | Out-String) -ForegroundColor Cyan
-#}
-
-if (gcm pshazz -ea SilentlyContinue) {
-    pshazz init 'xpando' 
-}
--- Scripts/tail.ps1
+++
@@ -1,9 +0,0 @@
-[cmdletbinding()]
-param(
-  [paraMetEr(ValueFromPipeline)]
-  [object]$path,
-  [int]$lineCount = 5
-)
-
-cat $path -tail $lineCount -wait
-
